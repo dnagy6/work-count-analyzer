ed65318363358bcc0eaadc13395424611e8457d6
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-30 14:42:38 -0400

    Added Header Docstring

---
+++ Lab10_dnagy6-1.py
@@ -0,0 +1,9 @@
+"""
+Program Name: Word Count Analyzer
+Author: Dakota Nagy
+Purpose: To analyze user-selected text files, calculate word frequencies using 
+         Object-Oriented Programming, and output a sorted alphabetical report.
+Starter Code: None used. Developed entirely from assignment guidelines.
+Date: July 5, 2026
+
+"""
