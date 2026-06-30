a2e26a486028930016f6d60d747bd05fd0f432b9
Dakota Nagy <dnagy6@student.cscc.edu>
2026-06-30 14:51:46 -0400

    Created WordAnalyzer class and provided future TODO comments for execution

--- Lab10_dnagy6-1.py
+++ Lab10_dnagy6-1.py
@@ -7,3 +7,26 @@ Starter Code: None used. Developed entirely from assignment guidelines.
 Date: July 5, 2026
 
 """
+
+from pathlib import Path
+import string
+
+class WordAnalyzer:
+    def __init__(self, filepath: str):
+        self._filepath = Path(filepath)
+        self._frequencies = {}
+
+    def process_file(self) -> bool:
+        # TODO: text cleaning will go here
+        pass
+
+    def print_report(self):
+        #TODO: abc sorting and printing the report will go here
+        pass
+
+def main():
+    #TODO: menu loop, input validation, and class execution
+    pass
+
+if __name__ == "__main__":
+    main()
